378f67ad5ae567ba9672f89eccabb5717c249df9
Ashley M Mader <ashley.mader12@gmail.com>
2020-04-05 15:05:42 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,5 +1,4 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
-("Mr. Chocolate", 3 , "Male", "Brown", "Gentle", 1);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 3 , "Male", "Brown", "Gentle", 1);
 ("Rowdy", 1 , "Male", "Brown", "Agressive", 1);
 ("Tabitha", 2 , "Female", "Black", "Gentle", 1);
 ("Sergeant Brown", 6 , "Male", "Brown", "Gentle", 1);
