68d4601bdb6b52b4effdbde9528f40349207c199
Ashley M Mader <ashley.mader12@gmail.com>
2020-04-05 15:13:29 +0000

    Automatically backed up by Learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -2,8 +2,9 @@ def selects_all_female_bears_return_name_and_age
  "SELECT name, age FROM bears WHERE gender='F'"
 end
 
-def selects_all_bears_names_and_orders_in_alphabetical_order
-  "SELECT names, FROM bears ORDER BY name DESC"
+def selects_all
+  _bears_names_and_orders_in_alphabetical_order
+  "SELECT names FROM bears ORDER BY name DESC"
 end
 
 def selects_all_bears_names_and_ages_that_are_alive_and_order_youngest_to_oldest
